97c8a3712186d98c10b7687540251585b8d6beb9
michael <mkigotho2@gmail.com>
2015-08-19 01:39:12 +0300

    opening a chat app project

---
+++ app/src/main/java/com/example/mike/simplechat/ChatActivity.java
@@ -0,0 +1,29 @@
+package com.example.mike.simplechat;
+
+import android.app.Application;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+
+public class ChatActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_chat, menu);
+        return true;
+    }
+
+  
+}
